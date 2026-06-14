2164e6390d76d64100a96ef6f79e9e8bd8455d03
Ильяс Авзалов <ilasavzalov822@gmail.com>
2026-06-14 14:00:45 +0500

    Create 1.py

---
+++ Полиморфизм/1.py
@@ -0,0 +1,32 @@
+class Selector:
+    def __init__(self, values):
+        self.values = values
+    
+    def get_odds(self):
+        return [num for num in self.values if num % 2 != 0]
+    
+    def get_evens(self):
+        return [num for num in self.values if num % 2 == 0]
+
+
+
+values = [11, 12, 13, 14, 15, 16, 22, 44, 66]
+selector = Selector(values)
+odds = selector.get_odds()
+evens = selector.get_evens()
+print(' '.join(map(str, odds)))
+print(' '.join(map(str, evens)))
+
+values = [6, 6, 0, 4, 8, 7, 6, 4, 7, 5]
+selector = Selector(values)
+odds = selector.get_odds()
+evens = selector.get_evens()
+print(' '.join(map(str, odds)))
+print(' '.join(map(str, evens)))
+
+values = []
+selector = Selector(values)
+odds = selector.get_odds()
+evens = selector.get_evens()
+print(' '.join(map(str, odds)))
+print(' '.join(map(str, evens)))
